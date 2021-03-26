ff7d5513130320d7db19630844aa85b9066463d2
safecorners <safeconers.mobile@gmail.com>
2021-03-27 06:04:12 +0900

    add inflearn examples

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Kubernetes
 
 - [쿠버네티스 시작하기2/e](http://acornpub.co.kr/book/kubernetes-up-and-running-2e)
+
---
+++ inflearn/ingress/nginx-install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.27.1/deploy/static/mandatory.yaml
